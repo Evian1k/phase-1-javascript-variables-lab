1bddd4a764a8e2921a297d3ac240177ef8cd11a4
Evian1k <emmanuelevian@gmail.com>
2025-03-04 12:14:11 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber'; // Must be a constant
+let mostProfitableNeighborhood = 'Chelsea'; // Use Let
+let companyCeo = 'Susan Smith'; // Use Let
